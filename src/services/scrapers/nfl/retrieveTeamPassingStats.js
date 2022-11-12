4a3fe1ba04c85a57d2c8203130f5af7ecc15e2c9
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-11-12 09:47:12 -0600

    refactor function

--- src/services/scrapers/nfl/retrieveTeamPassingStats.js
+++ src/services/scrapers/nfl/retrieveTeamPassingStats.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-async function scrapeTeamPassingStats(){
+async function scrapeTeamPassingStats() {
     const nflUrl = 'https://www.nfl.com/stats/team-stats/offense/passing/2022/reg/all';
     const date = new Date();
     const browser = await puppeteer.launch({});
@@ -9,27 +9,29 @@ async function scrapeTeamPassingStats(){
     await page.goto(nflUrl);
 
     // get passing headers 
-      const passingStatsHeaders = await page.$$eval('th', columns => {
-        return Array.from(columns, column => column.innerText); 
-      });
-      
+    const passingStatsHeaders = await page.$$eval('th', columns => {
+        return Array.from(columns, column => column.innerText);
+    });
+
     // get passing stats 
     const passingStats = await page.$$eval('tr', rows => {
         return Array.from(rows, row => {
-          const columns = row.querySelectorAll('td');
-          return Array.from(columns, column => column.innerText);
+            const columns = row.querySelectorAll('td');
+            return Array.from(columns, column => column.innerText);
         });
-      });
-      
-      passingStats[0] = passingStatsHeaders;
-      console.log(passingStats);
+    });
 
-            //Save data to JSON fike 
-            fs.writeFileSync('src/data/passingStats' + date.toISOString() + '.json', JSON.stringify(passingStats), (err) => {
-                if (err) { throw err; } else {
-                    console.log("File Saved");
-                }
-              });
+    passingStats[0] = passingStatsHeaders;
+    console.log(passingStats);
+
+    //Save data to JSON file 
+    const passingStatsNewFileName = 'src/data/passingStats' + date.toISOString() + '.json';
+    try {
+        fs.writeFileSync(passingStatsNewFileName, JSON.stringify(passingStats));
+        console.log('File Saved!');
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = { scrapeTeamPassingStats };
