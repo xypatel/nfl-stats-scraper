4eeba9fda3258a7de8352e289a9b51008f979f17
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-11-12 09:37:47 -0600

    rafactor structure

---
+++ src/services/scrapers/nfl/retrieveTeamPassingStats.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+async function scrapeTeamPassingStats(){
+    const nflUrl = 'https://www.nfl.com/stats/team-stats/offense/passing/2022/reg/all';
+    const date = new Date();
+    const browser = await puppeteer.launch({});
+    const page = await browser.newPage();
+    await page.goto(nflUrl);
+
+    // get passing headers 
+      const passingStatsHeaders = await page.$$eval('th', columns => {
+        return Array.from(columns, column => column.innerText); 
+      });
+      
+    // get passing stats 
+    const passingStats = await page.$$eval('tr', rows => {
+        return Array.from(rows, row => {
+          const columns = row.querySelectorAll('td');
+          return Array.from(columns, column => column.innerText);
+        });
+      });
+      
+      passingStats[0] = passingStatsHeaders;
+      console.log(passingStats);
+
+            //Save data to JSON fike 
+            fs.writeFileSync('src/data/passingStats' + date.toISOString() + '.json', JSON.stringify(passingStats), (err) => {
+                if (err) { throw err; } else {
+                    console.log("File Saved");
+                }
+              });
+}
+
+module.exports = { scrapeTeamPassingStats };
