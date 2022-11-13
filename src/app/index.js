bce6bd66f5bce65b67d201ab21a9e9da34d71b69
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-11-12 20:30:50 -0600

    (refactor) split db connection function

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,10 @@
-const { connectPgDb } = require('../services/database/connectPgDb');
+const { connectPgDb, disconnectPgDb } = require('../services/database/connectPgDb');
 const { scrapeTeamPassingStats } = require('../services/scrapers/nfl/retrieveTeamPassingStats');
 
 async function run() {
   connectPgDb();
   scrapeTeamPassingStats();
+  disconnectPgDb();
 }
 
 run();
--- src/services/database/connectPgDb.js
+++ src/services/database/connectPgDb.js
@@ -1,20 +1,35 @@
 const { Client } = require("pg")
 const dotenv = require("dotenv")
+
 dotenv.config()
- 
+const client = new Client();
+
 const connectPgDb = async () => {
     try {
-        const client = new Client();
         await client.connect();
-        const dbname = 'teamPassingStats';
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const disconnectPgDb = async () => {
+    try {
+        await client.end();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createStatsDb = async () => {
+    try {
+        const dbname = 'stats';
         await client.query(`DROP DATABASE IF EXISTS ${dbname};`);
-        // await client.query(`CREATE DATABASE ${dbname};`);
+        await client.query(`CREATE DATABASE ${dbname};`);
         await client.end();
     } catch (error) {
         console.log(error);
     }
 }
  
-connectPgDb();
 
-module.exports = { connectPgDb };
+module.exports = { connectPgDb, disconnectPgDb, createStatsDb };
