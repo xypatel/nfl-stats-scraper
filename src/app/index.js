d1cc46354e29c9dfa7027de9376f1e5e927e93a5
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-12-02 18:57:01 -0600

    remove node-pg

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,7 @@
-const { connectPgDb, disconnectPgDb } = require('../services/database/connectPgDb');
 const { scrapeTeamPassingStats } = require('../services/scrapers/nfl/retrieveTeamPassingStats');
 
 async function run() {
-  connectPgDb();
   scrapeTeamPassingStats();
-  disconnectPgDb();
 }
 
 run();
