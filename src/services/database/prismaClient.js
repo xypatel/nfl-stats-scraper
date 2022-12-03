81912116702cef90e6c3dac186c7b721b9437d40
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-12-03 02:39:54 -0600

    cleanup

--- src/services/database/prismaClient.js
+++ src/services/database/prismaClient.js
@@ -7,28 +7,6 @@ async function getPassingStats() {
     console.log("Prisma find many from Postgres DB");
     console.log(allPassingStats);
 }
-async function testPut() {
-    await prisma.passing_stats.create({
-        data: {
-            Team: 'test',
-            Att: 0,
-            Cmp: 0,
-            Cmp_perc: 0,
-            Yds_per_Att: 0,
-            Pass_Yds: 0,
-            TD: 0,
-            INT: 0,
-            Rate: 0,
-            first: 0,
-            first_perc: 0,
-            over_20: 0,
-            over_40: 0,
-            Lng: 0,
-            Sck: 0,
-            SckY: 0,
-        }
-    })
-}
 
 async function putPassingStats() {
     for (let year = 1997; year <= 2022; year++) {
