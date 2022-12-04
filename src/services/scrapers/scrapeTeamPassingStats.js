e937a07bd6ef606fe24a4b467979221aa2010338
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2022-12-03 23:59:14 -0600

    resolve timeout issue

--- src/services/scrapers/scrapeTeamPassingStats.js
+++ src/services/scrapers/scrapeTeamPassingStats.js
@@ -4,6 +4,7 @@ async function scrapeTeamPassingStats(year) {
     const nflUrl = 'https://www.nfl.com/stats/team-stats/offense/passing/' + year + '/reg/all';
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
     await page.goto(nflUrl);
 
     const passingStats = await page.$$eval('tr', rows => {
